f115936609b991f581055af4f56941eb3547ba6d
Olga Botvinnik <olga.botvinnik@gmail.com>
2019-09-08 22:33:58 -0700

    ahh typing is so crazy .. when do you '&' and when do you not? I have no clue

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,22 @@ extern crate clap;
 use clap::{App, load_yaml, value_t};
 
 mod extract;
+mod classify_reads;
 
 fn main() -> Result<(), ExitFailure> {
 
     let yml = load_yaml!("must.yml");
     let m = App::from_yaml(yml).get_matches();
+
+    // Vary the output based on how many times the user used the "verbose" flag
+    // (i.e. 'myprog -v -v -v' or 'myprog -vvv' vs 'myprog -v'
+    match m.occurrences_of("v") {
+        0 => println!("No verbose info"),
+        1 => println!("Some verbose info"),
+        2 => println!("Tons of verbose info"),
+        3 | _ => println!("Don't be crazy"),
+    }
+    let verbosity = m.occurrences_of("v");
 
     match m.subcommand_name() {
         Some("extract") => {
@@ -22,6 +33,27 @@ fn main() -> Result<(), ExitFailure> {
             // Convert ksize string argument to integer
             let ksize: u8 = value_t!(cmd, "ksize", u8).unwrap_or_else(|e| e.exit());
             extract::extract_kmers(sequence_files, ksize);
+        }
+        Some("classify") => {
+            let cmd = m.subcommand_matches("compute").unwrap();
+            let sequence_files = cmd
+                .values_of("sequence_files")
+                .map(|vals| vals.collect::<Vec<_>>())
+                .unwrap();
+
+            // Convert ksize string argument to integer
+            let ksize: u8 = value_t!(cmd, "ksize", u8).unwrap_or_else(|e| e.exit());
+
+            let coding_kmer_file = cmd.value_of("coding_kmers").unwrap();
+            let non_coding_kmer_file = cmd.value_of("noncoding_kmers").unwrap();
+
+            // Convert ksize string argument to integer
+            let ksize: u8 = value_t!(cmd, "ksize", u8).unwrap_or_else(|e| e.exit());
+
+            classify_reads::classify(sequence_files,
+                                     coding_kmer_file,
+                                     non_coding_kmer_file,
+                                     ksize, verbosity);
         }
         _ => {
             println!("{:?}", m);
